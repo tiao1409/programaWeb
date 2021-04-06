84d73e0c2f86a3ac1b7170bfa30237b9a05d37c1
tiao <ssnkia@gmail.com>
2021-04-06 20:30:10 -0300

    Adicionando validacao de nome vazio no formulario

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function helloWorld() {
     var inputs =  document.getElementsByTagName('input');
     var input = inputs[0];
-    var nome = input.value;
+    var nome = input.value.trim();
+    if (nome === ''){
+      alert('O campo nome está vazio, por favor digite se nome.');
+      return;
+    }
     console.log('Nome:', nome);
     window.alert('Hello World,' + nome + '!');
     input.value = '';
